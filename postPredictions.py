1629e275cbf68bf922d90b49c214121bed193762
Jason Lin <uscjlin@gmail.com>
2017-09-09 17:40:07 -0400

    minor fix

--- postPredictions.py
+++ postPredictions.py
@@ -3,10 +3,11 @@ import json
 
 url = 'https://data.chastiser11.hasura-app.io/v1/query'
 
+# Sample row input
 symbol = "GOOGL"
 market = "NASDAQ"
 numDays = "3"
-results = [234, 450, 390] 
+results = [234, 450, 390, 403] 
 
 payload = {"type": "insert", "args":{"table": "stockforecast", "objects": [{"stocksymbol": symbol, "market": market, "period": numDays, "predictions": results }]}}
 headers = {"content-type": "application/json", "authorization": "Bearer wj7fmf21w6lvu0l4u7vmdef1tqo0cykn"}
